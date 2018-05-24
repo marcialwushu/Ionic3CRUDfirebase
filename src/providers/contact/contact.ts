84be2668b05c679492a9466c1614091d1add9b49
marcialwushu <marcialwushu@gmail.com>
2018-05-24 13:22:41 -0300

    create getAll function in provider

--- src/providers/contact/contact.ts
+++ src/providers/contact/contact.ts
@@ -1,17 +1,35 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
-/*
-  Generated class for the ContactProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ContactProvider {
+  private PATH = 'contacts/';
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ContactProvider Provider');
+  constructor(private db: AngularFireDatabase) {
+
+  }
+
+  getAll(){
+    return this.db.list(this.PATH)
+      .snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({
+          key: c.payload.key, ...c.payload.val()
+        }));
+      })
+  }
+
+  get(key: string){
+
+  }
+
+  save(contact: any){
+
+  }
+
+  remove(key: string){
+
   }
 
 }
